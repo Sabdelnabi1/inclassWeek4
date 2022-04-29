d8c1ccf0be5a56d40f970ba9aaf8ee71bb166e25
R2H <r2h@AdminisatorsMBP.lan>
2022-04-29 14:19:04 -0400

    In class assignment solutions

---
+++ script.js
@@ -0,0 +1,32 @@
+//1 Convert the following function into an arrow function named "arrowMyFunction".
+const myFunction = function (num1, num2) {
+    return num1 + num2;
+};
+//Here is the shorter version
+const arrowMyFunction = (num1, num2) => (num1 + num2);
+
+console.log(arrowMyFunction(3, 5))
+
+
+
+//2 Convert the following function into a one-lined arrow function without a return statement (the return is implicit) . Name the function arrowAdd100
+//here is the shorter version without a return statement
+const add100 = function (money) {
+    return money + 100;
+};
+
+const arrowAddOneHundred = (money) => money + 100;
+
+console.log(arrowAddOneHundred(100))
+
+
+//3 Create an arrow function that takes TWO arguments, a string and a single letter.  The function will count the number of times the specified letter occurs in the string. **Not case sensitive
+// Examples:
+// countLetters('Hello my name is John', 'i') returns 1
+// countLetters('Hello my name is John', 'h') returns 2
+// countLetters('We love this city a lot, sometimes', 'e') returns 4
+//first I created a function that takes a string and a letter. Then I had it find how many times a specfic letter comes up in there and subtract by one because now it is an array
+let letterCount = (string, find) => (string.split(find)).length - 1;
+
+//then I console logged it to count how many times P comes up in the string
+console.log(letterCount("Peter Pan Picks Pickles", "P"));
